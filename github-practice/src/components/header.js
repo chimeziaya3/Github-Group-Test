d5d17b079399f23fc9ce6f0eb2dd47da89a2385e
Madeline <madelinenelsonfolkersen@gmail.com>
2017-05-22 11:37:21 -0400

    created components folder, added header component

---
+++ github-practice/src/components/header.js
@@ -0,0 +1,13 @@
+class Header extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>
+          This is the header component!
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
