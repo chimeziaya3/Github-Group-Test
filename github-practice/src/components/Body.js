d9852446c060928a42322656f371db507cc57ff8
[Chimezie Anyaoha] <[chimezie.anyaoha@gmail.com]>
2017-05-22 11:49:12 -0400

    add Body component

---
+++ github-practice/src/components/Body.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Body extends Component {
+    render() {
+        return (
+            <body>
+                <h1>this sucks</h1>
+            </body>
+        )
+    }
+}
+
+export default Body;
